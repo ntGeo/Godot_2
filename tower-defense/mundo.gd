1acca222b5f2670242c0e2ed6adf75c09826fa8b
Dmartin1989 <danielenriquemartin1@gmail.com>
2026-07-07 21:27:08 -0300

    Update mundo.gd

--- tower-defense/mundo.gd
+++ tower-defense/mundo.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var enemigo_scene = preload("res://enemigo.tscn")
+	var enemigo_scene = preload("res://Scenes/enemigo.tscn")
 	var enemigo = enemigo_scene.instantiate()
 
 	enemigo.path_follow = $Camino/Recorrido
