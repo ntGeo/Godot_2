f7cbba6ef24de426b1d96dcec2bf5d205ba16632
Dmartin1989 <danielenriquemartin1@gmail.com>
2026-07-08 20:17:01 -0300

    codigo de disparo sin rotación

--- tower-defense/Assets/Scripts/torre_1.gd
+++ tower-defense/Assets/Scripts/torre_1.gd
@@ -1,12 +1,26 @@
 extends Area2D
 
+const BALA = preload("res://Scenes/proyectil.tscn") 
+var tiempo_disparo = 0.1
+var tiempo_trascurrido = 0.1
+var objetivo = null
+var nueva_bala = BALA.instantiate()
+
+func _physics_process(delta: float) -> void:
+	var  enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target_enemy = enemies_in_range.front()
 
 
+#func _on_body_entered(body: Node2D) -> void:
+	#pass
 
-func _on_body_entered(body: Node2D) -> void:
-		print(enemigo en area)
-	
-	
 func shoot():
-	const BALA = preload("res://Scenes/proyectil.tscn")
-	$CollisionShape2D/Sprite2D/punto_disparo
+	var bala = BALA.instantiate() as Area2D
+	bala.global_position = %punto_disparo.global_position
+	bala.global_rotation = %punto_disparo.global_rotation
+	get_tree().current_scene.add_child(bala)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
--- tower-defense/Scenes/proyectil.gd
+++ tower-defense/Scenes/proyectil.gd
@@ -1,14 +1,26 @@
 extends Area2D
 
-var distancia_viajada = 0
+const VELOCIDAD = 1000
+const RANGO = 1200
 
-func _physics_process(delta):
-	const VELOCIDAD = 1000
-	const RANGO = 1200
-	
-	var direccion = Vector2.RIGHT.rotated(rotation)
+var distancia_viajada = 0
+var direccion = Vector2.RIGHT.rotated(rotation)
+
+
+
+# funcion del movimiento de la bala
+func _physics_process(delta: float) -> void:
 	position += direccion * VELOCIDAD * delta
 	
+	
+	# Desaparece el proyectil a los 1200px
 	distancia_viajada += VELOCIDAD * delta
 	if distancia_viajada > RANGO:
 		queue_free()
+
+
+# Daño
+func _on_body_entered(body:) -> void:
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
